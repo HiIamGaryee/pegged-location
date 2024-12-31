7c0394220474ae78636956621794ce1e0dc5c7d2
joee <sumjoee17@gmail.com>
2024-12-31 18:36:15 +0800

    error in the FlowlogIn

--- src/app/components/DisplayUser.tsx
+++ src/app/components/DisplayUser.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-// import Link from "next/link";
-// import { useAuth } from "../context/AuthContext";
-import Image from "next/image";
+import Link from "next/link";
+import React from 'react';
+import { useAuth } from "../context/AuthContext";
+import useCurrentUser from "@hooks/use-current-user.hook";
 
 export default function DisplayUser() {
-  // const { userID, username, windowHeight } = useAuth();
-
+  const {OKXlogIn, userID, username, windowHeight } = useAuth();
+  //const [Flowuser, FlowloggedIn, FlowlogIn, FlowlogOut] = useCurrentUser ();
+  const [FlowlogIn] =useCurrentUser();
   return (
     <div className="max-w-2xl mx-auto px-4 py-8 flex flex-col items-center justify-center min-h-screen text-center space-y-4">
       <h1 className="text-4xl font-bold  text-primary">
@@ -19,21 +21,12 @@ export default function DisplayUser() {
 
       {/* <Link href="/info"> */}
       <div className="mt-4 py-2 px-4 border-2 border-primary bg-primary text-white rounded-full hover:bg-primaryDark transition duration-150 ease-in-out">
-        <button>Connect Wallet</button>
+        <button onClick={OKXlogIn}>OKX Wallet</button>
       </div>
-      <div className="w-full items-center">
-        <div
-          className="inline-block ml-2 cursor-pointer "
-          style={{
-            position: "relative",
-            width: 200,
-            height: 200,
-            objectFit: "contain",
-          }}
-        >
-          <Image src="/main.png" alt="Submit" layout="fill" objectFit="cover" />
-        </div>
+      <div className="mt-4 py-2 px-4 border-2 border-primary bg-primary text-white rounded-full hover:bg-primaryDark transition duration-150 ease-in-out">
+        <button onClick={FlowlogIn}>Flow Cadence Wallet</button>
       </div>
+      
       {/* </Link> */}
       {/* <Link href="/function">
           <div className="mt-4 py-2 px-4 border-2 border-primary text-primary rounded-full hover:bg-gray-200 transition duration-150 ease-in-out">
